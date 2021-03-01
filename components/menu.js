8d6476b3cfb1d8e4a108c13add0d50ee4fd8d358
Lambert Chan <42985344+lambert-chan@users.noreply.github.com>
2021-03-01 09:50:44 -0800

    Hard-coded the menu to not be reliant on Apollo, but losing dynamicness

--- components/menu.js
+++ components/menu.js
@@ -61,38 +61,115 @@ const NavMenu = ({ className }) => {
     )
 }
 
-const GetRCEFCSubMenuItems = (data, parent) => {
-    let id = parent?.id
-    console.log(data)
-    console.log(parent)
-    return data.map(item => {
-        if (item?.parentId === id) {
-            let slug = item?.path.split('/')
-            //Hash for join us slides
-            let hash = slug > 2 ? item?.path : `/${slug[1]}#${slug[2]}`
-            if (parent?.label !== 'About') {
-                hash = item?.path
-            }
-            console.log(hash)
-            return (
-                <Menu.Item key={item?.id}>
-                    <Link href={hash}>{item?.label}</Link>
-                </Menu.Item>)
+/**
+ * Hard Coded RCEFC Menu to bypass dynamic (but slower) loading of Apollo
+ * @date 02/26/2021
+ * @author Lambert Chan
+ * @param {classname} classname pass the design on
+ */
+const RCEFCMenu = ({ className }) => {
+    let menu_items = [
+        {
+            title: 'Join Us',
+            slug: 'join-us',
+            children: [
+                {
+                    title: 'Worship',
+                    slug: 'worship'
+                },
+                {
+                    title: 'Sermon Recording List',
+                    slug: 'sermon-recording-list'
+                },
+                {
+                    title: 'Children',
+                    slug: 'children'
+                },
+                {
+                    title: 'Awana',
+                    slug: 'awana'
+                },
+                {
+                    title: 'Crossroads Fellowship',
+                    slug: 'crossroads-fellowship'
+                },
+                {
+                    title: 'Young Adults',
+                    slug: 'yads'
+                },
+                {
+                    title: 'Adults Fellowship',
+                    slug: 'adults'
+                },
+                {
+                    title: 'Small Groups',
+                    slug: 'small-groups'
+                },
+            ]
+        },
+        {
+            title: 'About',
+            slug: 'about',
+            children: [
+                {
+                    title: 'History',
+                    slug: 'history'
+                },
+                {
+                    title: 'Our God Given Purpose',
+                    slug: 'purpose-statement'
+                },
+                {
+                    title: 'Vision Statement',
+                    slug: 'vision'
+                },
+                {
+                    title: 'Core Values',
+                    slug: 'core-values'
+                },
+                {
+                    title: 'Pastoral Team',
+                    slug: 'pastoral-team'
+                },
+            ]
+        },
+        {
+            title: 'Give',
+            slug: 'give',
+            children: [
+                {
+                    title: 'Online Offering',
+                    slug: 'online'
+                }
+            ]
+        },
+        {
+            title: 'Contact Us',
+            slug: 'contacts',
+            children: [
+                {
+                    title: 'Location',
+                    slug: 'location'
+                }
+            ]
         }
-    })
-}
-
-const RCEFCMenu = ({ className, menuItems }) => {
-    let primary_menu_items = menuItems.filter(item => !item.parentId)
-    let sub_menu_items = menuItems.filter(item => item.parentId)
+    ]
     return (
         <Menu className={headerStyles.main_menu + " " + className} mode="horizontal" triggerSubMenuAction='click'>
-            {primary_menu_items.map(menuItem =>
-                <SubMenu item={menuItem?.label} key={menuItem?.id} name={menuItem?.label}>
-                    {GetRCEFCSubMenuItems(sub_menu_items, menuItem)}
-                </SubMenu>)}
+            {
+                menu_items.map(primary =>
+                    <SubMenu key={primary.slug} title={primary.title}>
+                        {primary.children.map(sub => 
+                            <Menu.Item key={sub.slug}>
+                                <Link href={`/${primary.slug}` + (primary.slug !== 'about' ? '/' : '#') + sub.slug}>
+                                    {sub.title}
+                                </Link>
+                            </Menu.Item>
+                        )}
+                    </SubMenu>
+                )
+            }
         </Menu>
     )
 }
-
-export default NavMenu
+export default RCEFCMenu
